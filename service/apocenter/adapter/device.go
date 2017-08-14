a15fbdd1c879d6dc8567bc40696c0f56b98bdef2
wans <guyun.hy@gmail.com>
2017-08-14 23:51:34 +0800

    Add apocenter adapter package

---
+++ service/apocenter/adapter/device.go
@@ -0,0 +1,11 @@
+package adapter
+
+import (
+	"fxservice/service/apocenter/domain"
+)
+
+// GetDeviceBySN
+// Get device by sn
+func GetDeviceBySN(sn string) (error, *domain.Device) {
+	return nil, nil
+}
